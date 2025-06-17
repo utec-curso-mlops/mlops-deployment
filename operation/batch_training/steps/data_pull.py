fdb1940ece1a849675302ae20fe86f32a20cb935
Ricardo <ricki212011@gmail.com>
2025-06-16 23:16:32 -0500

    test batch

--- operation/batch_training/steps/data_pull.py
+++ operation/batch_training/steps/data_pull.py
@@ -1,9 +1,7 @@
 import sys 
 import os
 
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from ..batch_training_utils import TRACKING_SERVER_ARN, DEFAULT_PATH
+from operation.batch_training.batch_training_utils import TRACKING_SERVER_ARN, DEFAULT_PATH
 from sagemaker.workflow.function_step import step
 
 
