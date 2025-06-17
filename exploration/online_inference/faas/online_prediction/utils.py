f41c98f476559589e77c37e66d7705f7ea2abbe4
Ricardo <ricki212011@gmail.com>
2025-06-16 23:39:30 -0500

    delete

--- exploration/online_inference/faas/online_prediction/utils.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def get_username():
-    with open("/opt/ml/metadata/resource-metadata.json", "r") as f:
-        app_metadata = json.loads(f.read())
-        space_name = app_metadata["SpaceName"]
-    return space_name
--- operation/batch_training/steps/data_pull.py
+++ operation/batch_training/steps/data_pull.py
@@ -4,9 +4,9 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '...')))
 print(sys.path)
 print(os.path.join(os.path.dirname(__file__)))
-print((os.path.join(os.path.dirname(__file__), '...')))
+print((os.path.join(os.path.dirname(__file__), '..')))
 
-from batch_training.batch_training_utils import TRACKING_SERVER_ARN, DEFAULT_PATH
+from ..batch_training_utils import TRACKING_SERVER_ARN, DEFAULT_PATH
 from sagemaker.workflow.function_step import step
 
 
