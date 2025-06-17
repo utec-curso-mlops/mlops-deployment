86da5ec79e402a9adc48bf0c26aed107965a3b74
Ricardo <ricki212011@gmail.com>
2025-06-17 16:47:45 -0500

    add test

---
+++ operation/online_inference_faas/tests/test.py
@@ -0,0 +1,11 @@
+import boto3
+import json
+from faas_utils import FUNCTION_NAME
+
+def test_lambda_invocation():
+    client = boto3.client('lambda')
+    payload = {"body": {"data": [0,237.10,487.97,504.7,7878.1]}}
+    response = client.invoke(FunctionName=FUNCTION_NAME,Payload=json.dumps(payload),LogType='Tail')
+    response_payload = json.loads(response['Payload'].read().decode('utf-8'))
+    assert abs(eval(response_payload["body"])["prediction"] - 0.0007983) < 0.000001
+
