6f4d66ddb87cef697d4b393707edc250048a4f18
Ricardo <ricki212011@gmail.com>
2025-06-16 23:25:05 -0500

    add syspath

--- operation/batch_training/steps/data_pull.py
+++ operation/batch_training/steps/data_pull.py
@@ -1,6 +1,8 @@
 import sys 
 import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '...')))
+
 from operation.batch_training.batch_training_utils import TRACKING_SERVER_ARN, DEFAULT_PATH
 from sagemaker.workflow.function_step import step
 
