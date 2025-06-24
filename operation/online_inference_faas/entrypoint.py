73d0a93a317d573777f846bb17bf0eccc47dce5c
Ricardo <ricki212011@gmail.com>
2025-06-24 17:19:03 -0500

    adding dynamo

--- operation/online_inference_faas/entrypoint.py
+++ operation/online_inference_faas/entrypoint.py
@@ -2,6 +2,8 @@ import mlflow
 import json
 import os
 import pandas as pd
+from decimal import Decimal
+import boto3
 
 # Load model
 model_name = os.getenv("MODEL_NAME")
@@ -11,6 +13,7 @@ model_uri = f"models:/{model_name}/{model_version}"
 # Set tracking server
 tracking_server_arn = os.getenv("TRACKING_SERVER_ARN")
 print(tracking_server_arn)
+logging_table = os.getenv("LOG_TABLE")
 
 # Set tracking server
 mlflow.set_tracking_uri(tracking_server_arn)
@@ -18,12 +21,28 @@ model = mlflow.xgboost.load_model(model_uri)
 FEATURES = ['card_present', 'trx_vel_last_1mths', 'trx_vel_last_2mths',
                 'amt_vel_last_1mths', 'amt_vel_last_2mths']
 
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
 def lambda_handler(event, context):
     if not isinstance(event, dict):
         event = eval(event)
     df = pd.DataFrame([event['body']['data']])
     pred = model.predict_proba(df[FEATURES])[:, 1][0]
-    
+    # Logging prediction
+    table = dynamodb.Table(logging_table)
+
+    response = table.put_item(
+        Item= {
+            "transaction_id" : event['body']['data']["transacion_id"],
+            "cod_month": event['body']['data']["cod_month"],
+            "card_present": event['body']['data']["card_present"],
+            "trx_vel_last_1mths": event['body']['data']["trx_vel_last_1mths"],
+            "trx_vel_last_2mths": event['body']['data']["trx_vel_last_2mths"],
+            "amt_vel_last_1mths": Decimal(str(event['body']['data']["amt_vel_last_1mths"])),
+            "amt_vel_last_2mths": Decimal(str(event['body']['data']["amt_vel_last_2mths"]))
+        }
+    )
+
     return {
         'statusCode': 200,
         'body': json.dumps({'prediction': float(pred)})
